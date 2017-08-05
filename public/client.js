5586ca41d785c171d3993f19b287942cbc390730
Alex Jung <alexandermjung89@gmail.com>
2017-08-04 20:52:59 -0500

    buttons functioning

---
+++ public/client.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    var inputOne = $('#inputOne').val();
+    var inputTwo = $('#inpuTwo').val();
+    var operator ='';
+
+console.log(inputOne);
+console.log(inputTwo);
+
+$('#add').on('click', function(){ 
+console.log('add clicked');
+
+})//end of add
+$('#subtract').on('click', function(){ 
+console.log('add subtract');
+})//end of subtract
+$('#multiply').on('click', function(){ 
+console.log('add multiply');
+})//end of multiply
+$('#divide').on('click', function(){ 
+console.log('add divide');
+})//end of divide
+
+
+
+
+})//end of document.ready
